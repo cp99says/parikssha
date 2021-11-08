5e28a69a62e8fb526440beffe61b3e7c6f950efc
“sd2001” <im.swarnabha2001@gmail.com>
2021-11-08 10:28:12 +0530

    🚧 bash files for setting up server

---
+++ ML-server/init.sh
@@ -0,0 +1,10 @@
+poetry run python -m spacy download en_core_web_sm
+poetry run python -m spacy download en
+poetry run pip install git+https://github.com/boudinfl/pke.git
+poetry run python -c "import nltk; nltk.download('punkt')"
+poetry run python -m nltk.downloader stopwords
+sudo apt-get update
+sudo apt-get install libleptonica-dev 
+sudo apt-get install tesseract-ocr tesseract-ocr-dev
+sudo apt-get install libtesseract-dev
+sudo apt-get install tesseract-ocr
